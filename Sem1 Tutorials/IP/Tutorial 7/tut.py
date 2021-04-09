870c75f0cd7dc4be7cafae8ee10c9e8bcd4c41c1
aflah02 <72096386+aflah02@users.noreply.github.com>
2021-04-09 20:34:42 +0530

    Delete Sem1 Tutorials/IP directory

--- Sem1 Tutorials/IP/Tutorial 7/tut.py
+++
@@ -1,7 +0,0 @@
-txt = "H\te\tl\tl\to"
-
-print(txt)
-print(txt.expandtabs())
-print(txt.expandtabs(2))
-print(txt.expandtabs(4))
-print(txt.expandtabs(10))
--- Sem1 Tutorials/IP/Tutorial 9/magic_module.py
+++
@@ -1,2 +0,0 @@
-def find_magic_num(x,y,z):
-    return ((x**2+y)%z)**5
