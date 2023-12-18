54c3e00381b202704bd76bf4a86f761b096db1c3
Anirudh-S-Kumar <anirudh.skumar.03@gmail.com>
2023-12-18 22:55:54 +0530

    created an init readme file, added table to new docs

---
+++ init_readme.md
@@ -0,0 +1,13 @@
+# IIITD Resources
+
+Contributors:
+
+<a href="https://github.com/aflah02/SemWiseResourcesIIIT/graphs/contributors">
+  <img src="https://contrib.rocks/image?repo=aflah02/SemWiseResourcesIIIT" />
+</a>
+
+Repository to store all the resources for IIITD courses. You can use either the [website](https://aflah02.github.io/SemWiseResourcesIIIT/) or the README.md file to access the resources.
+
+If you want to add resources but confused about how to start, please refer to this [guide](https://github.com/aflah02/SemWiseResourcesIIIT/blob/main/CONTRIBUTING.md)
+
+Note: The process has recently changed (December 2023) and the changes now need to go to `resources.yaml` and not to `README.md` directly
--- yaml_to_docs.py
+++ yaml_to_docs.py
@@ -3,6 +3,40 @@ import yaml
 # Constants
 YAML_FILE_PATH:str = 'resources.yaml'
 DOCS_DIRECTORY:str = 'docs/'
+INIT_README:str = 'init_readme.md'
+FINAL_README:str = 'docs/index.md'
+
+def build_index(yaml_dump:str) -> None:
+    # open the init_readme.md file
+    with open(INIT_README, 'r') as f:
+        readme = f.read()
+    
+    # start creating the table
+    readme += "\n| Semester | Courses |"
+    readme += "\n| --- | --- |\n"
+
+    # add the semesters
+    for semester in yaml_dump.keys():
+        sem_link = semester.replace(' ', '-').lower()
+        readme += f"| [{semester}]({sem_link}) | "
+        cnt = 0
+        # add the courses
+        for course in yaml_dump[semester].keys():
+            cnt += 1
+            course_link = course.lower()
+            # replace & with empty string
+            course_link = sem_link + "#" + course_link.replace('&', '')
+            readme += f"[{course}]({course_link}) "
+            if (cnt == 10):
+                readme += '<br><br>'
+                cnt = 0
+
+        # add the new line
+        readme += "|\n"
+    
+    # write to README.md
+    with open(FINAL_README, 'w') as f:
+        f.write(readme)
 
 
 def build_docs(yaml_dump:str) -> None:
@@ -36,6 +70,7 @@ def yaml_to_docs() -> None:
     with open(YAML_FILE_PATH, 'r') as f:
         yaml_dump = yaml.load(f, Loader=yaml.FullLoader)
 
+    build_index(yaml_dump)
     build_docs(yaml_dump)
 
 if __name__ == '__main__':
